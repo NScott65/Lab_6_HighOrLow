f2f18d5063b4e5f5db6300b282488c788e119b70
NScott65 <scott.613865@swl.k12.oh.us>
2023-10-09 16:39:52 -0400

    hi

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
+import java.util.Scanner;
+import java.lang.Math;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+
+        //variables
+        int userNum = 0;
+        int randomNum = 0;
+        boolean done = false;
+
+        do{
+            System.out.println("Enter a number 1-10: ");
+            if(scan.hasNextInt()){
+                userNum = scan.nextInt();
+                if(userNum >= 1 && userNum <= 10){
+                    randomNum = (int)(Math.random() * 10) + 1;
+                    System.out.println("My number was " + randomNum + ".");
+                    if(randomNum > userNum){
+                        System.out.println("Your number was too low.");
+                    }else if(randomNum < userNum){
+                        System.out.println("Your number was too high.");
+                    }else if(randomNum == userNum){
+                        System.out.println("You got it!");
+                    }
+                    done = true;
+                }else{
+                    scan.nextLine();
+                    System.out.println("Your input was invalid. Try Again.");
+                }
+            }else{
+                scan.nextLine();
+                System.out.println("Your input was invalid. Try Again.");
+            }
+        }while(!done);
     }
 }
